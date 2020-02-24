d0062a234f359bd8df2247cdb62d3c17fa9f14ee
Bryant Su <bsu@Bryants-MacBook-Pro.local>
2020-02-24 18:39:52 -0500

    router set up

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Header from '../Header'
+import Body from '../Body'
+
+import './Layout.css'
+
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  )
+}
+
+export default Layout
