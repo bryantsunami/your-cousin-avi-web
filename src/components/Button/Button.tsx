1d27096c5bfcc141b6ad9e2d7991226eb5ed1003
Albert Zhu <zalbert9@gmail.com>
2020-03-02 18:46:35 -0500

    Implement Button
    - followed Button tutorial
    - uses hooks
    - doesn't jump
    - does cool animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, FC } from 'react';
+import { useSpring, animated } from 'react-spring';
 
-const Button = () => <button>I'm a butt</button>;
+type TButtonTypes = 'round' | 'square' | 'on-hover';
+
+interface IButtonProps {
+  clickAction: () => any;
+  buttonType?: TButtonTypes;
+}
+
+const Button: FC<IButtonProps> = ({ clickAction, buttonType, children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [showLoader, setShowLoader] = useState(false);
+
+  const fadeOutProps = useSpring({ opacity: showLoader ? 1 : 0 });
+  const fadeInProps = useSpring({ opacity: showLoader ? 0 : 1 });
+
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (ref.current && ref.current.getBoundingClientRect().width) {
+      setWidth(ref.current.getBoundingClientRect().width);
+    }
+    if (ref.current && ref.current.getBoundingClientRect().height) {
+      setHeight(ref.current.getBoundingClientRect().height);
+    }
+  }, [children]);
+
+  useEffect(() => {
+    if (isLoading) {
+      setShowLoader(true);
+    }
+
+    if (!isLoading && showLoader) {
+      const timeout = setTimeout(() => {
+        setShowLoader(false);
+      }, 400);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [isLoading, showLoader]);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    clickAction().then(() => {
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      ref={ref}
+      style={
+        width && height
+          ? {
+              width: `${width}px`,
+              height: `${height}px`,
+            }
+          : {}
+      }
+    >
+      {showLoader ? (
+        <animated.div style={fadeOutProps}>Loading</animated.div>
+      ) : (
+        <animated.div style={fadeInProps}>{children}</animated.div>
+      )}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  buttonType: 'round',
+};
 
 export default Button;
